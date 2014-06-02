0325a3b7096c2c0c6f8364ea7c6aa48d8c3e799b
Mark <mark@praxitar.com>
2014-06-02 14:24:50 +0100

    fix up and multi-select added

---
+++ lib/active_admin/inputs/filter_multiple_select_input.rb
@@ -0,0 +1,19 @@
+module ActiveAdmin
+  module Inputs
+    class FilterMultipleSelectInput < Formtastic::Inputs::SelectInput
+
+     include FilterBase
+
+     def input_name
+        "#{super}_in"
+      end
+
+      def extra_input_html_options
+        {
+          :class => 'select2-input', :multiple => true
+        }
+      end
+
+    end
+  end
+end
--- lib/active_admin/select2.rb
+++ lib/active_admin/select2.rb
@@ -1,1 +1,5 @@
 require 'active_admin/select2/engine'
+require 'active_admin/select2/filter_select_input_extension.rb'
+require 'active_admin/inputs/filter_multiple_select_input.rb'
+
+ActiveAdmin::Inputs::FilterSelectInput.send :include, ActiveAdmin::Select2::Inputs::FilterSelectInputExtension
