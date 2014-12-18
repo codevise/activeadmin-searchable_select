7212d35f54b42171d3fea7f0a6dda934f7d41b19
Davor Bagarić <davor.bagaric@rebellis.hr>
2014-12-18 20:32:51 +0100

    Update select2.rb

--- lib/activeadmin/select2.rb
+++ lib/activeadmin/select2.rb
@@ -2,4 +2,4 @@ require 'activeadmin/select2/engine'
 require 'activeadmin/select2/filter_select_input_extension.rb'
 require 'activeadmin/inputs/filter_select2_multiple_input.rb'
 
-ActiveAdmin::Inputs::Filter::SelectInput.send :include, ActiveAdmin::Select2::Inputs::FilterSelectInputExtension
+ActiveAdmin::Inputs::Filters::SelectInput.send :include, ActiveAdmin::Select2::Inputs::FilterSelectInputExtension
