235e8a1f53ecd0be9ea064669c16aeda532ed5de
Tim Fischbach <tfischbach@codevise.de>
2017-10-16 17:43:24 +0200

    Remove legacy inputs
    * We want to add inputs to `ActiveAdmin::Inputs(::Filters)` instead of
    `Formtastic::Inputs`.
    
    * `Filter*Input` convention is no longer supported by Active Admin 1.x.
    
    * We do not want to patch all select filter inputs by default.

--- lib/activeadmin-select2.rb
+++ lib/activeadmin-select2.rb
@@ -1,7 +1,2 @@
 require 'activeadmin/select2'
 
-require 'activeadmin/inputs/filter_select2_multiple_input'
-require 'formtastic/inputs/select2_input'
-require 'formtastic/inputs/select2_multiple_input'
-require 'formtastic/inputs/select2_tags_input'
-
--- lib/activeadmin/select2.rb
+++ lib/activeadmin/select2.rb
@@ -1,5 +1,2 @@
 require 'activeadmin/select2/engine'
-require 'activeadmin/select2/filter_select_input_extension.rb'
-require 'activeadmin/inputs/filter_select2_multiple_input.rb'
 
-ActiveAdmin::Inputs::Filters::SelectInput.send :include, ActiveAdmin::Select2::Inputs::FilterSelectInputExtension
