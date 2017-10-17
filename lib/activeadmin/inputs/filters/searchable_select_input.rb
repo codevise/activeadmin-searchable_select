41ccd87148a0a75f8b969b2c955734a78439d9f1
Tim Fischbach <tfischbach@codevise.de>
2017-10-17 11:37:34 +0200

    Add yardopts file

--- lib/activeadmin/inputs/filters/searchable_select_input.rb
+++ lib/activeadmin/inputs/filters/searchable_select_input.rb
@@ -3,8 +3,8 @@ module ActiveAdmin
     module Filters
       # Searchable select input type for ActiveAdmin filters.
       #
-      # @see ActiveAdmin::Select2::SelectInputExtension for list of
-      # available options.
+      # @see ActiveAdmin::SearchableSelect::SelectInputExtension
+      #   SelectInputExtension for list of available options.
       class SearchableSelectInput < SelectInput
         include SearchableSelect::SelectInputExtension
       end
--- lib/activeadmin/inputs/searchable_select_input.rb
+++ lib/activeadmin/inputs/searchable_select_input.rb
@@ -2,8 +2,8 @@ module ActiveAdmin
   module Inputs
     # Searchable select input type for ActiveAdmin filters.
     #
-    # @see ActiveAdmin::Select2::SelectInputExtension for list of
-    # available options.
+    # @see ActiveAdmin::SearchableSelect::SelectInputExtension
+    #   SelectInputExtension for list of available options.
     class SearchableSelectInput < Formtastic::Inputs::SelectInput
       include SearchableSelect::SelectInputExtension
     end
